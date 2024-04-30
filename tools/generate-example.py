3eba4f5d11ea1853fcaf2c98ebd6c30570260182
Kazuya Takei <myself@attakei.net>
2024-04-30 21:35:47 +0900

    tools: Change proc to generate html

--- tools/generate-example.py
+++ tools/generate-example.py
@@ -9,8 +9,8 @@ from docutils.core import publish_doctree, publish_from_doctree
 from rst_budoux import parse_all_sentences
 from rst_budoux.cli.html import CustomHTMLWriter
 
-ROOT = Path(__file__).parent.parent
-SOURCE = "`あなた <http://example.com>`_ に寄り添う **最先端** のテクノロジー。"
+HERE = Path(__file__).parent
+ROOT = HERE.parent
 
 
 def get_env_dir() -> Path:  # noqa: D103
@@ -27,9 +27,10 @@ def cleanup_document(document: nodes.document) -> nodes.document:
     all_text: list[nodes.Text] = list(document.findall(nodes.Text))
     for text in all_text:
         content = text.astext()
-        if not (content.startswith(" ") or content.endswith(" ")):
+        trimmed = content.strip()
+        if content == trimmed:
             continue
-        new_text = nodes.Text(content.strip())
+        new_text = nodes.Text(trimmed)
         text.parent.insert(text.parent.index(text), new_text)
         new_text.parent.remove(text)
     return document
@@ -38,14 +39,30 @@ def cleanup_document(document: nodes.document) -> nodes.document:
 def main():  # noqa: D103
     env_dir = get_env_dir()
     # Generate HTML
-    document = cleanup_document(publish_doctree(SOURCE))
+    document = cleanup_document(
+        publish_doctree(source=(HERE / "example-rst.txt").read_text())
+    )
     with (env_dir / "without-budoux.html").open("w") as fp:
-        fp.write(publish_from_doctree(document, writer_name="html5").decode("utf-8"))
+        fp.write(
+            publish_from_doctree(
+                document,
+                writer_name="html5",
+                settings_overrides={
+                    "template": str(HERE / "template.txt"),
+                },
+            ).decode("utf-8")
+        )
     budoux_parser = budoux.load_default_japanese_parser()
     document = parse_all_sentences(budoux_parser, document)
     with (env_dir / "with-budoux.html").open("w") as fp:
         fp.write(
-            publish_from_doctree(document, writer=CustomHTMLWriter()).decode("utf-8")
+            publish_from_doctree(
+                document,
+                writer=CustomHTMLWriter(),
+                settings_overrides={
+                    "template": str(HERE / "template.txt"),
+                },
+            ).decode("utf-8")
         )
 
 
