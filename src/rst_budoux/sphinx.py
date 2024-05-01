8bc2c67da520c9578e056401b3ddfd2334e66a3d
Kazuya Takei <myself@attakei.net>
2024-05-02 02:49:05 +0900

    feat(sphinx): Extension re-parse doctree and render word-break nodes

--- src/rst_budoux/sphinx.py
+++ src/rst_budoux/sphinx.py
@@ -1,11 +1,21 @@
 """Implementation as Sphinx-extension."""
 
+import budoux
 from sphinx.application import Sphinx
+from sphinx.util.docutils import nodes
 
-from . import __version__
+from . import WordBreak, __version__, parse_all_sentences
+from .writers import html
+
+
+def _insert_word_break(app: Sphinx, doctree: nodes.document) -> nodes.document:
+    parser = budoux.load_default_japanese_parser()
+    return parse_all_sentences(parser, doctree)
 
 
 def setup(app: Sphinx):  # noqa: D103
+    app.add_node(WordBreak, html=(html.visit_word_break, html.depart_word_break))
+    app.connect("doctree-read", _insert_word_break)
     return {
         "version": __version__,
         "env_version": 1,
--- tests/test_sphinx.py
+++ tests/test_sphinx.py
@@ -8,3 +8,12 @@ from sphinx.testing.util import SphinxTestApp
 def test__it(app: SphinxTestApp):
     """Test that it passes with rst-budoux extension."""
     app.build()
+
+
+@pytest.mark.sphinx("html")
+def test_parsed_document(app: SphinxTestApp):
+    """Test that it passes with rst-budoux extension."""
+    app.build()
+    html = app.outdir / "ja.html"
+    data = html.read_text()
+    assert "\u200b" in data
